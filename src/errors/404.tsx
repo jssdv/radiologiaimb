771650177c7e2818c53b46e3c04ca5aaa32bf7a4
jss <andrestorve@gmail.com>
2025-08-12 12:14:17 -0500

    Update 404.tsx

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <section class="bg-red-100 text-slate-700 p-8 rounded-md">
       <h2 class="text-2xl">404: Not Found</h2>
-      <p class="mt-4">It's gone 😞!</p>
+      <p class="mt-4">It's goneeeee 😞!</p>
     </section>
   );
 }
